5effdc04bc9314cd1b5eaff4c6994be245616916
Дерюгин Глеб <deryugin.gleb@gmail.com>
2019-12-13 12:44:48 +0300

    Create Ellipse.cs

---
+++ 3-lesson/Ellipse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace _3_lesson
+{
+    class Ellipse : Shape
+    {
+        public Ellipse(double A1, double A2, double B1, double B2) : base(A1, A2, B1, B2)
+        { }
+        public override double Distance(PointF point1)
+        {
+            return 1;
+        }
+    }
+}
